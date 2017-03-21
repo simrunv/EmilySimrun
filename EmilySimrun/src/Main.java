49bbe3ddfb617e802c4fce50c0063422adc09a78
emily_wang <emily_wang@student.wayland.k12.ma.us>
2017-03-21 13:13:43 -0400

    attempt to move unicorn

--- EmilySimrun/src/Main.java
+++ EmilySimrun/src/Main.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by simrun_virkud on 3/15/17.
@@ -7,12 +11,49 @@ import java.awt.*;
 public class Main extends JPanel{
 
     public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
-    private Pepsi unicorn, candy, candy2;
+    private Pepsi unicorn, candy;
+    private Timer timer;
+    private boolean[] keys;
 
     public Main(){
         unicorn = new Unicorn();
         candy = new Candy(100,100);
-        candy2 = new Candy(200,200);
+        keys = new boolean[500];
+
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //move the unicorn
+                    if(keys[KeyEvent.VK_SPACE]){
+                        unicorn.setDir(90);
+                        unicorn.update();
+                        keys[KeyEvent.VK_SPACE] = false;
+                        //If you want to have the user have to push the key each move use the false.
+                    }
+
+                repaint();
+                }
+        });
+        timer.start();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = true;
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = false;
+            }
+        });
     }
 
     public void paintComponent(Graphics g) {
@@ -21,9 +62,6 @@ public class Main extends JPanel{
 
         unicorn.draw(g2);
         candy.draw(g2);
-        candy2.setPic("smallcandy.png", 90);
-        candy2.draw(g2);
-
     }
 
 
